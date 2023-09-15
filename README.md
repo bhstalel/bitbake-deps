a06b1a3ebc365fbdd2522e99c78778551938209d
Talel BELHADJSALEM <talel.hajsalem@sofia-technologies.com>
2023-09-15 13:28:05 +0100

    First commit with bitbake-deps.sh script Detailed README about the shell script .gitignore for pn-buildlist generated files

--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# bitbake-recipe-deps
-Do something on a recipe's dependencies rather than the recipe itself
+# Bitbake On Recipe Dependencies
+
+This project provides tools that perform custom tasks or full build on a given recipe's dependencies which are recipes also.
+
+## What ?
+
+The idea is the following:
+
+1. Generate a recipe's dependencies list
+2. Remove the recipe itself from the list to avoid `bitbake`ing it
+3. Loop through the list and run the full or given task on the recipe
+
+## How ?
+
+This project provide two scripts that do the same thing:
+
+* `bitbake-deps.sh`: Can be ran from anywhere.
+* `bitbake-deps`: Must be copied to `poky/bitbake/bin` to be able to use `bitbake` library
+
+## Shell Solution
+
+* Usage:
+
+```sh
+./bitbake-deps.sh target [task]
+```
+
+The shell script provide a `DEBUG` level that you can activate by setting the variable `ENABLE_DEBUG` to `1`:
+
+```sh
+ENABLE_DEBUG=1 ./bitbake-deps.sh core-image-minimal
+```
+
+When enabling `DEBUG`, `bitbake` output will be shown along side the `[DEBUG]` log message.
+
+## Use case examples
+
+* Fetch all dependencies of an image:
+
+```sh
+./bitbake-deps.sh core-image-minimal do_fetch
+```
+
+* Build all dependencies of a given package:
+
+```sh
+./bitbake-deps.sh virtual/kernel
+```
+
+## TODO
+
+- [ ] Complete the Python `bitbake-dep` script
---
+++ bitbake-deps.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+
+#
+#   Exit global function
+#
+#   ~ Export current PID
+export THIS_PID=$$
+#   ~ Trap the <TERM> signal command
+trap "exit 1" TERM
+trap "exit 1" SIGINT
+#
+die(){
+    kill -s TERM ${THIS_PID}
+}
+
+ENABLE_DEBUG=${ENABLE_DEBUG:-0}
+#
+#   Log function
+#   
+#   Arguments:
+#       > $1 = level
+#       > $2 = message
+#
+do_log(){
+    local level=$1
+    local content=$2
+
+    case ${level} in
+        "info"|"INFO")
+            echo -e "\e[32m[INFO] ${content}\e[0m";;
+        "warn"|"WARN"|"WARNING")
+            echo -e "\e[33m[WARN] ${content}\e[0m";;
+        "debug"|"DEBUG")
+            [ ${ENABLE_DEBUG} -eq 1 ] && (
+                echo -e "\e[34m[DEBUG] ${content}\e[0m"
+            );;
+        "error"|"ERROR")
+            echo -e "\e[31m[ERROR] ${content}\e[0m"
+            die
+        ;;
+        *)
+            echo -e "\e[31m[ERROR] ${level} not supported\e[0m"
+            die
+        ;;
+    esac
+}
+
+PN_BUILDLIST="pn-buildlist"
+
+#
+# Check if source is set or not
+#
+do_check_env(){
+    do_log debug "Checking if the environment is set .."
+    BITBAKE_PATH=$(which bitbake)
+    [[ (-z ${BUILDDIR}) || (-z ${BBPATH}) || (-z ${BITBAKE_PATH}) ]] && (
+        do_log error "The bitbake environment is not set yet!"
+    )
+}
+
+#
+# Generate dependency list of the given recipe
+# and remove it from the list itself to avoid bitbaking it.
+#
+do_gen_dep_list(){
+    local recipe=$1
+    do_log info "Generating ${PN_BUILDLIST} for recipe: ${recipe}"
+
+    bitbake -g $recipe > /dev/null
+    sed -i "/$recipe/d" ${PN_BUILDLIST}
+}
+
+#
+# Run the given bitbake task on the given recipe's dep list
+#
+do_bitbake(){
+    local task=$1
+    local command="bitbake ${recipe}"
+
+    do_log debug "Running bitbake on $(wc -l ${PN_BUILDLIST}) recipes .."
+    while read recipe; do
+        [ -z ${task} ] && (
+            do_log info "Running full recipe ${recipe}"
+        )|| (
+            do_log info "Running task ${task} on recipe ${recipe}"
+            command+=" -c ${task}"
+        )
+        # Disable output if DEBUG is disabled
+        [ ${ENABLE_DEBUG} -eq 0 ] && command+=" > /dev/null"
+
+        # Run command
+        eval ${command}
+    done < ${PN_BUILDLIST}
+}
+
+do_usage(){
+    do_log error "Usage: $1 recipe [task]"
+}
+
+#
+#   Check if recipe and task are given
+#
+do_sanity(){
+    do_log debug "Checking passed arguments .."
+    [[ ($# -eq 0) || ($# > 2) ]] && do_usage $0
+}
+
+#
+#   Main function
+#
+do_main(){
+    local recipe=$1
+    local task=$2
+
+    do_sanity $@
+    do_check_env
+    do_gen_dep_list ${recipe}
+    do_bitbake ${task}
+}
+
+do_main $@
